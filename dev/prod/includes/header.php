96f2b4d68cc0d66ae56985aa08830188c821f27f
Jorge Cortés <jorge.ecro@gmail.com>
2015-07-27 21:38:25 -0500

    quienes, favicon

--- dev/prod/includes/header.php
+++ dev/prod/includes/header.php
@@ -4,9 +4,9 @@
 		<meta charset="utf-8">
 		<title>Mezcal Fruto del Sol</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<!-- link rel="icon" href="favicon.png" type="image/png" sizes="32x32" --> 
+		<link rel="icon" href="favicon.png" type="image/png" sizes="32x32"> 
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-		<meta name="description" content="“Expresiones de Sabor” es una empresa 100% mexicana, que en sus Mezcales destaca la devoción, respeto y trato justo con los productores artesanos Oaxaqueños que por generaciones han elaborado este maravilloso producto, conservando la tradición para México y un regalo para el mundo entero.">
+		<meta name="description" content="Expresiones de Sabor es una empresa 100% mexicana, que en sus Mezcales destaca la devoción, respeto y trato justo con los productores artesanos Oaxaqueños que por generaciones han elaborado este maravilloso producto, conservando la tradición para México y un regalo para el mundo entero.">
 		<meta name="keywords" content="Expresiones de Sabor, Oaxaca, Mezcal, Fruto del Sol">
 		<meta name="author" content="jorge.ecro@gmail.com">
 	</head>
@@ -17,8 +17,8 @@
 			<div class="content">
 				<a href="index.php"><div class="logo"></div></a>
 				<div class="yllw">
-					<div class="tw"><i class="fa fa-twitter"></i></div>
-					<div class="fb"><i class="fa fa-facebook"></i></div>
+					<div class="tw"><a href="https://twitter.com/frutodelsol" target="blank"><i class="fa fa-twitter"></i></a></div>
+					<div class="fb"><a href="https://www.facebook.com/mezcalfrutodelsol" target="blank"><i class="fa fa-facebook"></i></a></div>
 				</div>
 				<div class="menu">
 					<ul>
